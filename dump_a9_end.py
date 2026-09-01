0b00da92ef4faf3d3a5278587529e57e29b5fc28
Neverfinished005 <vablerudra007@gmail.com>
2026-09-01 18:59:57 +0530

    chore: clean up scratch analysis scripts for production release

--- dump_a9_end.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-url = "https://cdn.prod.website-files.com/5cff83ac2044e22cb8cf2f11/js/webflow.41526e95.e32c85395cd6ca97.js"
-req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-with urllib.request.urlopen(req) as resp:
-    data = resp.read().decode('utf-8', errors='ignore')
-
-idx = data.find('keyframe:70')
-print(data[idx:idx+4500])
